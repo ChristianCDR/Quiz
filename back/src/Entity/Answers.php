5f1b7c37716f52a27d024d93b78eff60ef97508e
A. Christian CLAUDE DIT REGENT <adonsou-christian.claude-dit-regent@epitech.eu>
2024-07-16 19:25:01 +0200

    mise en place react native et android

---
+++ back/src/Entity/Answers.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswersRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnswersRepository::class)]
+class Answers
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $answerText = null;
+
+    #[ORM\ManyToOne(inversedBy: 'answers')]
+    private ?questions $questions = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnswerText(): ?string
+    {
+        return $this->answerText;
+    }
+
+    public function setAnswerText(string $answerText): static
+    {
+        $this->answerText = $answerText;
+
+        return $this;
+    }
+
+    public function getQuestions(): ?questions
+    {
+        return $this->questions;
+    }
+
+    public function setQuestions(?questions $questions): static
+    {
+        $this->questions = $questions;
+
+        return $this;
+    }
+}
---
+++ back/src/Form/AnswersType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Answers;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnswersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('answerText')
+            ->add('questionId')
+            ->add('is_correct')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Answers::class,
+        ]);
+    }
+}
