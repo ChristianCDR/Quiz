615b3bc2a96209b27630129fb8d3c1cb7916ec65
A. Christian CLAUDE DIT REGENT <adonsou-christian.claude-dit-regent@epitech.eu>
2024-10-04 14:56:06 +0200

    profile screen

---
+++ front/app/ProfileScreen.tsx
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react';
+import { View, Text, ScrollView, Animated, Dimensions, StyleSheet } from 'react-native';
+
+export default function ProfileScreen () {
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+})  
