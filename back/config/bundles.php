0089beacbe9e00d84fbb6d54eec4973dac68d10f
A. Christian CLAUDE DIT REGENT <adonsou-christian.claude-dit-regent@epitech.eu>
2024-12-02 22:39:40 +0100

    backend in docker container

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
