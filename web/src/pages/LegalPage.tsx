a2354fff1852458f17bef6d9a1ca6257f6964cf7
A. Christian CLAUDE DIT REGENT <adonsou-christian.claude-dit-regent@epitech.eu>
2025-01-08 12:56:59 +0100

    starting legal page

---
+++ web/src/pages/LegalPage.tsx
@@ -0,0 +1,16 @@
+
+const LegalPage = () => {
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-blue-100">
+            <h1 className="text-4xl font-bold text-blue-600 mb-4">
+                Bienvenue sur ma première page React avec TypeScript et Tailwind CSS
+            </h1>
+            <p className="text-3xl text-gray-700">
+                Ceci est une page d'accueil utilisant React, TypeScript et Tailwind CSS !
+            </p>
+        </div>
+  );
+};
+
+export default LegalPage;
